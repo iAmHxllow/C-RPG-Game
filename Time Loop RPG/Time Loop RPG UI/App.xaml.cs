5131381218e5aa3e5e66a07906d092d1da28b9ad
David Adegeloye <davidadegeloye@gmail.com>
2025-04-05 07:44:52 +0100

    Created base file layout

---
+++ Time Loop RPG/Time Loop RPG UI/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Time_Loop_RPG_UI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
